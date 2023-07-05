6891ad7dfce2660e33aa571ac68c11c647f8dc2e
Ritwiz <ritwiz@nowpurchase.com>
2023-07-05 12:47:51 +0530

    form validation

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.forms import modelform_factory
 
 # Create your views here.
 from .models import Meeting, Room
@@ -12,5 +13,20 @@ def detail(request, id):
 def rooms_list(request):
     return render(request, 'meetings/rooms_list.html',
                   {'rooms': Room.objects.all()})
+
+
+MeetingForm = modelform_factory(Meeting, exclude=[])
+def new(request):
+
+    if request.method == "POST":
+        # form submitted, process data now
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("welcome")
+    else:
+        form = MeetingForm()
+    return render(request, "meetings/new.html",
+                  {"form": form})
 
 
